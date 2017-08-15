8824bf00f66f0c23b3ea2815967cd4ef9e039415
Ricardo Ruíz Velazco <gral.vacio@gmail.com>
2017-08-14 21:09:53 -0500

    login y estructura
    se crearon archivos independientes de js para el login de google

---
+++ assets/js/login_google.js
@@ -0,0 +1,19 @@
+function onSignIn(googleUser) {
+
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  $('.opciones_sesion').hide('slow',function(){
+    $('#img-logo').attr('src',profile.getImageUrl());
+    $('#img-pres').attr('src',profile.getImageUrl());
+    $('#nombre_pila').html(profile.getName().split(' ')[0])
+
+    $('#mi_perfil').show('fast')
+    $('main').html('Bienvenido <b>'+profile.getName().split(' ')[0]+'</b> con correo <b>'+profile.getEmail()+'</b>')
+
+  });
+
+  console.log('correo: ' + profile.getEmail()); // This is null if the 'correo' scope is not present.
+}
+//naljGdHLgiq4JJJ5ACQUhWhL
