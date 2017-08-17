f1986f13a5904ba1010a729e91e48741e88d5d85
Ricardo Ruíz Velazco <gral.vacio@gmail.com>
2017-08-16 20:43:15 -0500

    animacion box

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,5 +1,13 @@
 $(function(){
 
   $(".dropdown-button").dropdown({ constrainWidth: false });
-
+  $('.seccion').hover(function(){
+      var tmp_src = $(this).find('img').attr('src');
+      $(this).find('img').attr('src',$(this).find('img').attr('rel'));
+      $(this).find('img').attr('rel',tmp_src);
+  },function(){
+    var tmp_src = $(this).find('img').attr('src');
+    $(this).find('img').attr('src',$(this).find('img').attr('rel'));
+    $(this).find('img').attr('rel',tmp_src);
+  })
 })
